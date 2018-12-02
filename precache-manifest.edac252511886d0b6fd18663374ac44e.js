83df07999aaadf40b6990e655b0fcb4da49c8d4d
Stéphane Guinot <sguinot86@gmail.com>
2018-12-02 09:27:01 +0100

    Updates

---
+++ precache-manifest.edac252511886d0b6fd18663374ac44e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "dee7fd4cf9c86ac934e3cbed49eec352",
+    "url": "/hackatonHalloween/static/media/carambar.dee7fd4c.jpg"
+  },
+  {
+    "revision": "5f9e031b436d63879636",
+    "url": "/hackatonHalloween/static/css/main.a0756fc9.chunk.css"
+  },
+  {
+    "revision": "70a8197354f24ea50ef291ba5de9c43e",
+    "url": "/hackatonHalloween/static/media/dragibus.70a81973.jpg"
+  },
+  {
+    "revision": "9674fb37ac7633b8d4d4",
+    "url": "/hackatonHalloween/static/js/1.9674fb37.chunk.js"
+  },
+  {
+    "revision": "94ee4039948234eb80eb",
+    "url": "/hackatonHalloween/static/js/runtime~main.94ee4039.js"
+  },
+  {
+    "revision": "8734307cf0d6dccbfa709c041cf5d7b5",
+    "url": "/hackatonHalloween/static/media/coca.8734307c.jpg"
+  },
+  {
+    "revision": "5f9e031b436d63879636",
+    "url": "/hackatonHalloween/static/js/main.5f9e031b.chunk.js"
+  },
+  {
+    "revision": "3844d4a8acbee402028f208b1604314f",
+    "url": "/hackatonHalloween/static/media/croco.3844d4a8.jpg"
+  },
+  {
+    "revision": "16eb3175c16a087ac612f80eeed54e00",
+    "url": "/hackatonHalloween/static/media/schtroumpfs.16eb3175.jpg"
+  },
+  {
+    "revision": "e305c9cad0f78579e03c7dc97b1d47e5",
+    "url": "/hackatonHalloween/static/media/sucette.e305c9ca.jpg"
+  },
+  {
+    "revision": "9674fb37ac7633b8d4d4",
+    "url": "/hackatonHalloween/static/css/1.c46efe9c.chunk.css"
+  },
+  {
+    "revision": "3724b7200d22b9427f9063f5cc02a830",
+    "url": "/hackatonHalloween/index.html"
+  }
+];
